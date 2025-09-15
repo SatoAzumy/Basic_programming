00fc5f5a67d48b78e598ae81a0effaa2b7279cd5
SatoAzumy <66461406+SatoAzumy@users.noreply.github.com>
2025-09-15 13:20:37 +0300

    Add files via upload

---
+++ StructuresEnumerations/Task2/Task2/TaskPay.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <Windows.h>
+
+struct BankAccount
+{
+	int numPay;
+	std::string name;
+	double money;
+};
+
+void changeBalance(double &money, double newBalance) {
+	money=newBalance;
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus"); // установка вывода на кириллице	
+	SetConsoleCP(1251);// установка кодовой страницы win-cp 1251 в поток ввода
+
+	BankAccount newBankAccount = {};
+	double newBalance;
+
+	std::cout << "¬ведите номер счЄта: ";
+	std::cin >> newBankAccount.numPay;
+	std::cout << "¬ведите им€ владельца: ";
+	std::cin >> newBankAccount.name;
+	std::cout << "¬ведите баланс : ";
+	std::cin >> newBankAccount.money;
+	std::cout << "¬ведите новый баланс: ";
+	std::cin >> newBalance;
+	changeBalance(newBankAccount.money, newBalance);
+	std::cout << "¬аш счЄт: " << newBankAccount.name << ", " << newBankAccount.numPay << ", " << newBankAccount.money;
+}
+
