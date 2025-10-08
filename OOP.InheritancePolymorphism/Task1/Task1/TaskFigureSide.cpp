ad35bc7fb5bb4ec0b62f59a6e733e7dd08ba278d
SatoAzumy <66461406+SatoAzumy@users.noreply.github.com>
2025-10-08 15:52:43 +0300

    Add files via upload

---
+++ OOP.InheritancePolymorphism/Task1/Task1/TaskFigureSide.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+class Figure
+{
+public:
+	Figure(int length)
+	{
+		sideLength = length;
+	}
+	int getSideLength() { return sideLength; };
+	std::string getNameFigure() { return "Фигура: "; };
+private:
+	int sideLength;
+
+};
+class Triangle : public Figure
+{
+public:
+	Triangle() : Figure(3) {}
+	std::string getNameFigure() { return "Треугольник: "; };
+};
+class Quadrilateral : public Figure
+{
+public:
+	Quadrilateral() : Figure(4) {}
+	std::string getNameFigure() { return "Четырехугольник: "; };
+};
+int main()
+{
+	setlocale(LC_ALL, "rus"); // установка вывода на кириллице
+	SetConsoleCP(1251);// установка кодовой страницы win-cp 1251 в поток ввода
+	Figure figures = Figure(0);
+	Triangle triangles = Triangle();
+	Quadrilateral quadrilateral = Quadrilateral();
+	std::cout << "Количество сторон: " << std::endl;
+	std::cout << figures.getNameFigure() << figures.getSideLength() << std::endl;
+	std::cout << triangles.getNameFigure() << triangles.getSideLength() << std::endl;
+	std::cout << quadrilateral.getNameFigure() << quadrilateral.getSideLength() << std::endl;
+
+	return 0;
+}
